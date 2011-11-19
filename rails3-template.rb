a93c5faa4431caf053ef924f723a2b43ac176f39
Daniel Jonasson <daniel@guadeo.com>
2011-11-19 15:59:26 +0100

    Added some gems related to testing

--- rails3-template.rb
+++ rails3-template.rb
@@ -2,9 +2,7 @@ gem "execjs"
 gem "therubyracer"
 
 # Templating engine and CSS framework
-gem "haml"
 gem "haml-rails", :group => :development
-gem "sass"
 gem "hpricot", :group => :development
 gem "ruby_parser", :group => :development
 
@@ -18,18 +16,22 @@ gem "simple_form"
 gem "devise"
 gem "cancan"
 
-# rspec, factory girl, webrat, autotest for testing
+# rspec, factory girl, capybara, etc. for testing
 gem "rails3-generators", :group => [ :development ]
 gem "rspec-rails", :group => [ :development, :test ]
 gem "factory_girl_rails", :group => [ :development, :test ]
-gem  "rcov", :group => [ :development, :test ]
-gem "webrat", :group => :test
+gem "rcov", :group => [ :development, :test ]
+gem "capybara", :group => :test
 gem "ffaker", :group => :test
-gem "autotest", :group => :test
+gem 'spork', '~> 0.9.0.rc', :group => [:development, :test]
+gem 'guard-rspec', :group => [:development, :test]
+gem 'guard-spork', :group => [:development, :test]
+gem 'launchy', :group => [:development, :test]
+gem 'rspec_multi_matchers', :group => [:development, :test]
+gem 'random_data', :group => [:development, :test]
 
 # Back-office
 gem "activeadmin"
-#gem "meta_search"
 
 # Attachments
 gem "paperclip"
@@ -37,10 +39,11 @@ gem "paperclip"
 # Pagination
 gem "kaminari"
 
+
 run 'bundle install'
 
-rake "db:create", :env => :development
-rake "db:create", :env => :test
+run "bundle exec rake db:create", :env => :development
+run "bundle exec rake db:create", :env => :test
 
 generate 'nifty:layout --haml'
 remove_file 'app/views/layouts/application.html.erb' # use nifty layout instead
@@ -65,11 +68,10 @@ end
 run "echo '--format documentation' >> .rspec"
 
 generate "devise:install"
-generate "simple_form:install"
 generate "active_admin:install User"
 generate "devise:views users"
 
-rake "db:migrate"
+run "bundle exec rake db:migrate db:test:prepare"
 
 generate "cancan:ability"
 generate "active_admin:resource User"
@@ -80,8 +82,8 @@ route 'root :to => "home#index"'
 # clean up rails defaults
 remove_file 'public/index.html'
 remove_file 'rm public/images/rails.png'
-run 'cp config/database.yml config/database.example'
-run "echo 'config/database.yml' >> .gitignore"
+#run 'cp config/database.yml config/database.example'
+#run "echo 'config/database.yml' >> .gitignore"
 
 say <<-eos
   ============================================================================
